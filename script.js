eb72d4f6a63edae6a39e8734839f01a5fad8ae89
Moritz D <moam.dorner@gmail.com>
2024-12-18 11:17:30 +0100

    Fixed issue with Lively Integration

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-let Iterations = 2;
-let DropdownType = "Sierpinski Carpet";
+let Iterations = 3;
+let DropdownType = 0;
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 
 draw_fractal();
 
+window.addEventListener('resize', draw_fractal);
 
-window.addEventListener('livelyProperty', (event) => {
-    const { name, value } = event.detail;
-
-    if (name === "DropdownType") {
-        DropdownType = value;
-    } else if (name === "SliderIterations") {
-        Iterations = value;
+function livelyPropertyListener(name, val)
+{
+    //write name and val on the canvas
+    ctx.font = "30px Arial";
+    ctx.fillText(name + ": " + val, 10, 50);
+    
+    switch(name) {
+        case "DropdownType":
+            DropdownType = val;
+            break;
+        case "SliderIterations":
+            Iterations = val;
+            break;
     }
 
     draw_fractal();
-});
-
-window.addEventListener('resize', draw_fractal);
-
+}
 
 function resizeCanvas(canvas, ctx) {
     const devicePixelRatio = window.devicePixelRatio || 1;
@@ -39,12 +46,12 @@ async function draw_fractal()
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (DropdownType == "Sierpinski Triangle") {
+    if (DropdownType == 0) {
         const module = await import('./Backgrounds/sierpinski_triangle/main.js');
         module.draw(ctx, Iterations);
     }
 
-    else if (DropdownType == "Sierpinski Carpet") {
+    else if (DropdownType == 1) {
         const module = await import('./Backgrounds/sierpinski_carpet/main.js');
         module.draw(ctx, Iterations);
     }
